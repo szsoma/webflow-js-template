eb30dca0da6c3e4c917205b8ded2378ea4d81f5d
Soma Szoboszlai <somaszoboszlai@gmail.com>
2024-01-10 08:51:12 +0100

    template clear

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-import './styles/style.css'
-import Lenis from '@studio-freight/lenis'
+import "./styles/style.css";
+import Lenis from "@studio-freight/lenis";
 //import gsap from 'gsap'  // [import GSAP have to import in the function's js file]
 
-import home from './pages/home/home'
-import homeScroll from './pages/home/home-scroll'
+import home from "./pages/home/home";
+import homeScroll from "./pages/home/home-scroll";
 
 // import ScrollTrigger from 'gsap/ScrollTrigger'   [import GSAP have to import in the function's js file]
 // import home from './pages/home/home'  [imprort js file to main.js]
 
 // don't forget to register plugins
-//gsap.registerPlugin(ScrollTrigger, Flip); 
+//gsap.registerPlugin(ScrollTrigger, Flip);
 
 // Lenis SmoothScroll
 // function scroll()
@@ -21,13 +21,12 @@ import homeScroll from './pages/home/home-scroll'
 //             requestAnimationFrame(raf)
 //         }
 
-//     requestAnimationFrame(raf)    
+//     requestAnimationFrame(raf)
 // }
 // scroll();
 
-const isHome = document.querySelector('body').classList.contains('body--home')
-if(isHome)
-{
-    home()
-    homeScroll()
+const isHome = document.querySelector("body").classList.contains("body--home");
+if (isHome) {
+  home();
+  homeScroll();
 }
